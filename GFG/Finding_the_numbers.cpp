cff71b5086788f8baafc3f36cba67e0e082c7a65
Mithilesh0079 <49610150+Mithilesh0079@users.noreply.github.com>
2020-08-04 17:09:54 +0530

    Update Finding_the_numbers.cpp
    https://practice.geeksforgeeks.org/problems/finding-the-numbers/0/

--- GFG/Finding_the_numbers.cpp
+++ GFG/Finding_the_numbers.cpp
@@ -1,3 +1,5 @@
+//USing SORT O(NlogN)
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -31,6 +33,61 @@ int main()
 	        if(i==2*n+2-1)
 	        cout<<A[i];
 	    }
+	    
+	    cout<<endl;
+	    
+	    t--;
+	}
+	return 0;
+}
+
+
+// USing MAP in O(N) time
+
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t>0)
+	{
+	    int n;
+	    cin>>n;
+	    int A[2*n+2];
+	    
+	    for(int i=0; i<2*n+2; i++)
+	    cin>>A[i];
+	    
+	    map<int , int> pair;
+	    
+	    for(int i=0; i<2*n+2; i++)
+	     pair[A[i]]=0; 
+	    
+	    for(int i=0; i<2*n+2; i++)
+	    pair[A[i]]+= 1;
+	    
+	    vector<int > out(2,0);
+	    
+	    int count= 0;
+	    
+	    for(int i=0; i<2*n+2; i++)
+	    {
+	        if(pair[A[i]]%2!=0 && A[i]!= out[0])
+	        {
+	            out[count]= A[i];
+	            count++;
+	            if(count==2)
+	            break;
+	        }
+	    }
+	    if(out[0]>out[1])
+	    cout<<out[1]<<" "<<out[0];
+	    
+	    else
+	    cout<<out[0]<<" "<<out[1];
 	    
 	    cout<<endl;
 	    
