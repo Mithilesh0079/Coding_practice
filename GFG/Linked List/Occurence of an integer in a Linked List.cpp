b24120392bf9c6d831a0e29e6c65f93cfd6ec282
Mithilesh0079 <49610150+Mithilesh0079@users.noreply.github.com>
2020-08-11 00:17:37 +0530

    Create Occurence of an integer in a Linked List.cpp
    https://practice.geeksforgeeks.org/problems/occurence-of-an-integer-in-a-linked-list/1/

---
+++ GFG/Linked List/Occurence of an integer in a Linked List.cpp
@@ -0,0 +1,13 @@
+
+int count(struct node* head, int search_for)
+{
+    int count=0;
+  while(head!= NULL)
+  {
+      if(head->data==search_for)
+      count++;
+      
+      head= head->next;
+  }
+  return count;
+}
