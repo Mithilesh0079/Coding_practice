7857f41fa0d508a3ee7487264aa8c9d342b2008d
Mithilesh0079 <49610150+Mithilesh0079@users.noreply.github.com>
2020-08-26 18:14:15 +0530

    Create Merge K sorted linked lists.cpp
    https://practice.geeksforgeeks.org/problems/merge-k-sorted-linked-lists/1/

---
+++ GFG/Heap/Merge K sorted linked lists.cpp
@@ -0,0 +1,57 @@
+void shiftdown(vector<Node*> &arr, int i, int n)
+{
+    if(i<n)
+    {
+        int l= 2*i+1;
+        int min_index=i;
+        if(l<n && arr[l]->data<arr[i]->data)
+        min_index= l;
+        
+        int r= 2*i+2;
+        if(r<n && arr[r]->data<arr[min_index]->data)
+        min_index= r;
+        
+        if(min_index!= i)
+        {
+            swap(arr[i], arr[min_index]);
+            shiftdown(arr, min_index, n);
+        }
+    }
+}
+
+Node * mergeKLists(Node *arr[], int N)
+{
+   vector<Node*> heap;
+   for(int i=0; i<N; i++)
+   {
+       if(arr[i]!= NULL)
+       heap.push_back(arr[i]); 
+   }
+   for(int i= N/2; i>=0; i--)
+   shiftdown(heap, i, N);
+   
+   Node* ans = heap[0];
+   Node* top=  heap[0];
+     
+   while(heap.size()>=1)
+   {
+       if(heap[0]->next!=NULL)
+       {
+           heap.push_back(heap[0]->next);
+           swap(heap[0], heap[heap.size()-1]);
+           heap.pop_back();
+           shiftdown(heap, 0, heap.size());
+       }
+       else
+       {
+           swap(heap[0], heap[heap.size()-1]);
+           heap.pop_back();
+           if(heap.size()>=1)
+           shiftdown(heap, 0, heap.size());
+       }
+       top->next= heap[0];
+       top= top->next;
+   }
+   top->next=NULL;
+  return ans;
+}
